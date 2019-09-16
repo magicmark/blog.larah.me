a2117840d2ce7ee66aeac5197903206f7a37cfb5
Mark Larah <mark@larah.me>
2019-09-16 04:12:57 +0000

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-64692179-3',
       },
     },
     'gatsby-plugin-feed',
